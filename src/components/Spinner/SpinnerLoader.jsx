2aff480827fddb994d4295143413f3e125a96f2c
kiran <96545669+kiranj100@users.noreply.github.com>
2024-06-25 00:25:26 +0530

    Few feature changes for better UI

--- src/components/Spinner/SpinnerLoader.jsx
+++ src/components/Spinner/SpinnerLoader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function SpinnerLoader() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-blue-500"></div>
+      <div className="animate-spin rounded-full h-36 w-36 border-t-4 border-b-4 border-blue-500"></div>
     </div>
   );
 }

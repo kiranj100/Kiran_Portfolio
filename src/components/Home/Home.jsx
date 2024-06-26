32b32cb27df1fd5c774f9cf6cadbce57613747ac
kiran <96545669+kiranj100@users.noreply.github.com>
2024-06-26 14:39:33 +0530

    icons_change

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -25,7 +25,9 @@ function Home(props) {
           <button className=" mt-2 ">
             <Link
               to="https://drive.google.com/uc?export=download&id=14kfg982Z837l0kC-yaPau1zbz0cV2_Y1"
-              className=" text-xl text-center px-8 py-2 duration-200 max-w-none hover:text-white ring-2  hover:ring-blue-300 rounded-xl font-extrabold hover:bg-gradient-to-r from-indigo-600 to-blue-400 "
+              className=" text-xl text-center px-8 py-2 duration-200 max-w-none animate-pulse
+                 hover:text-white ring-2  hover:ring-blue-300 
+               rounded-xl font-extrabold bg-gradient-to-r from-indigo-600 to-blue-400 "
             >
               Resume
             </Link>

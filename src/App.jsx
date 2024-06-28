57489c63d778fd684228843b7fe574fde08b8ff3
kiran <96545669+kiranj100@users.noreply.github.com>
2024-06-28 23:57:25 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="" element={<Layout />}>
-        <Route path="/" element={<Home />} />
+        <Route index element={<Home />} />
         <Route path="/about" element={<About />} />
         {/* <Route path="/project" element={<Project />} /> */}
         <Route path="/contact" element={<Contact />} />

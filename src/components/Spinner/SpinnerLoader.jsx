7d61b089de4c405a5b39ac48b37262158cb9de7d
kiran <96545669+kiranj100@users.noreply.github.com>
2024-06-29 01:22:51 +0530

    commit

--- src/components/Spinner/SpinnerLoader.jsx
+++ src/components/Spinner/SpinnerLoader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function SpinnerLoader() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="animate-spin rounded-full h-56 w-56 border-t-8 border-b-8 border-blue-500"></div>
+      <div className="animate-spin rounded-full h-36 w-36 border-t-8 border-b-8 border-blue-500"></div>
     </div>
   );
 }
